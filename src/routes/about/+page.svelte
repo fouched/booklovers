0b7959138da6e2ab61cbcb1d52fc99aa47a37b46
Fouche du Preez <fouche@limehouse.co.za>
2024-01-15 21:01:07 +0200

    Added Bootstrap and Navbar

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Booklovers - About</title>
+</svelte:head>
+<div class="row">
+	<div class="col">
+		<h1>About Booklovers</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col">
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis ab, 
+			rem dolore minus itaque illo nam, reprehenderit a nihil tenetur quos non. 
+			Reprehenderit asperiores voluptas quod numquam delectus officiis iste?
+		</p>
+	</div>
+</div>
