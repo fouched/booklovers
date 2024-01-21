1973a9564f41a2541509e674e4a8f9b31a1663bc
Fouche du Preez <fouche@limehouse.co.za>
2024-01-21 18:03:03 +0200

    Added message store for flash msgs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script>
-	import 'bootstrap/dist/css/bootstrap.min.css'
-	import Nav from '$lib/components/Nav.svelte'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from '$lib/components/Nav.svelte'
+import messageStore from '$lib/stores/messages.stores'
+
+const hideFlash = () => {
+	messageStore.hide()
+}
 </script>
 <Nav />
 <main class="container">
+	<div class="row mt-3">
+			{#if $messageStore.show}
+			<div class="col">				
+					<div 
+							class:alert-danger={$messageStore.type === 'error'} 
+							class:alert-success={$messageStore.type === 'success'} 
+							class="alert alert-dismissible" role="alert">
+							<strong>{$messageStore.type === 'error' ? 'Error' : 'Success'}:</strong>
+							{$messageStore.message}
+							<button type="button" class="btn-close" on:click={hideFlash} aria-label="Close" />
+					</div>					
+			</div>
+			{/if}
+	</div>	
 	<slot />
 </main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+import { onDestroy } from 'svelte';
+import messageStore from '../lib/stores/messages.stores'
+
+const showSuccessFlash = () => {
+	messageStore.showSuccess('A success message')
+}
+
+const showErrorFlash = () => {
+	messageStore.showError('An error message')
+}
+
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={showSuccessFlash}>Succss</button>
+<button on:click={showErrorFlash}>Error</button>
